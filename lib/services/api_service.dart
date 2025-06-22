677f70edffff6c7f24d1b67100606958814123ff
Imane Azdoud <imane.azdoud01@gmail.com>
2025-06-22 19:59:06 +0100

    Updated API service to use new RapidAPI endpoint

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,46 +2,48 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String _baseUrl = 'https://v3.football.api-sports.io';
+  static const String _baseUrl = 'https://api-football-v1.p.rapidapi.com/v3';
   static const Map<String, String> _headers = {
-    'X-RapidAPI-Key': '8fb1ad15c6541eca62d7925e38da6a4d',
-    'X-RapidAPI-Host': 'v3.football.api-sports.io',
+    'x-rapidapi-key': '841a931021mshb8e199294a1a42cp15c4bbjsn9ed0c52ef22e',
+    'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
   };
 
   static Future<Map<String, dynamic>?> getPlayerDetails(int playerId) async {
     final url = Uri.parse('$_baseUrl/players?id=$playerId&season=2023');
     final resp = await http.get(url, headers: _headers);
     print('Player URL: $url => ${resp.statusCode}');
-    print('Body: ${resp.body}');
     final data = json.decode(resp.body);
-    if (data['results'] == 0) return null;
+    if (data['results'] == 0 || data['response'].isEmpty) return null;
     return data['response'][0];
   }
 
   static Future<Map<String, dynamic>?> getTeamDetails(int teamId) async {
     final url = Uri.parse('$_baseUrl/teams?id=$teamId');
     final resp = await http.get(url, headers: _headers);
+    if (resp.statusCode != 200) return null;
+
     final data = json.decode(resp.body);
-    if (data['results'] == 0) return null;
+    if (data['results'] == 0 || data['response'].isEmpty) return null;
 
     final team = data['response'][0]['team'];
     final country = team['country'];
 
-    final Map<String, String> countryFlagMap = {
-      'France': 'https://media.api-sports.io/flags/fr.svg',
-      'Spain': 'https://media.api-sports.io/flags/es.svg',
-      'Germany': 'https://media.api-sports.io/flags/de.svg',
-      'England': 'https://media.api-sports.io/flags/gb.svg',
-      'Portugal': 'https://media.api-sports.io/flags/pt.svg',
-      'USA': 'https://media.api-sports.io/flags/us.svg',
-      'Argentina': 'https://media.api-sports.io/flags/ar.svg',
-      'Saudi-Arabia': 'https://media.api-sports.io/flags/sa.svg',
-    };
-
     return {
       'name': team['name'],
       'country': country,
-      'countryFlag': countryFlagMap[country],
+      'logo': team['logo'],
     };
+  }
+
+  static Future<List<Map<String, dynamic>>?> getTeamsInLeague(
+      int leagueId, int season) async {
+    final url = Uri.parse('$_baseUrl/teams?league=$leagueId&season=$season');
+    final resp = await http.get(url, headers: _headers);
+    if (resp.statusCode != 200) return null;
+
+    final data = json.decode(resp.body);
+    if (data['results'] == 0 || data['response'].isEmpty) return null;
+
+    return List<Map<String, dynamic>>.from(data['response']);
   }
 }
